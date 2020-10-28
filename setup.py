ab0d356858ed740db68acc22199f726c66083eb5
babybabyCloud <364847242@qq.com>
2020-10-28 23:50:44 +0800

    exclude tests folder Fix import issue in test file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 setup(
     name='vali-helper',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(exclude='tests'),
     author='babybabyCloud',
     author_email='babybabycloud2020@gmail.com',
     url="https://gitee.com/babybabycloud/vali-helper"
